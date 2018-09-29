bdab3b30e2574c9bdde75fe6605d33eb141622f0
Isaac Morneau <isaacmorneau@gmail.com>
2018-09-28 20:53:33 -0700

    itoa jank before rewrite

--- main.asm
+++ main.asm
@@ -21,7 +21,7 @@ errpromptlen equ $-errprompt
 
 section .bss
 
-buff resb 6
+buff resb 7
 len resb 1
 
 section .text
@@ -33,6 +33,14 @@ _start:
     call print
 
     call getinput
+
+    mov edi, buff
+    ;input value already in eax
+    call itoa
+
+    push eax
+    push buff
+    call print
 
     jmp exit
 
@@ -64,46 +72,149 @@ getinput:
     mov eax, SYS_READ
     mov ebx, STDIN
     mov ecx, buff
-    mov edx, 6
+    mov edx, 7
     int 0x80
     ;eax has length
-    cmp eax, 6
-    jge errout
+
+    ;too long,
+    cmp eax, 7
+    je errout
+    ;too short
+    cmp eax, 1
+    je errout
 
     ;its terminated by \n
     mov edi, buff
     call atoi
+    ;eax has the int
 
     mov esp, ebp
     pop ebp
     ret
 
+
+itoa:
+    ;start at... something
+    ;next char
+    mov edx, 5
+    ;start at the end of the buffer
+    add edi, 5
+
+    ;check if its negative
+    cmp eax, 0
+    jge it_digits
+
+    ;start one higher for - sign at the end
+    inc edi
+    inc edx
+    ; add a negative sign
+
+
+it_digits:
+    cmp eax, 10000
+    jge it_5
+    dec edi
+    dec edx
+    cmp eax, 1000
+    jge it_4
+    dec edi
+    dec edx
+    cmp eax, 100
+    jge it_3
+    dec edi
+    dec edx
+    cmp eax, 10
+    jge it_2
+    dec edi
+    dec edx
+    cmp eax, 0
+    jge it_1
+    jmp errout
+
+it_5:
+    push edx
+    mov edx, 0
+    ;eax already is number to devide
+    mov ecx, 10
+    idiv ecx
+    ;convert digit to printable
+    add edx, 48
+    mov [edi], edx
+    dec edi
+    pop edx
+it_4:
+    push edx
+    mov edx, 0
+    ;eax already is number to devide
+    mov ecx, 10
+    idiv ecx
+    ;convert digit to printable
+    add edx, 48
+    mov [edi], edx
+    dec edi
+    pop edx
+it_3:
+    push edx
+    mov edx, 0
+    ;eax already is number to devide
+    mov ecx, 10
+    idiv ecx
+    ;convert digit to printable
+    add edx, 48
+    mov [edi], edx
+    dec edi
+    pop edx
+it_2:
+    push edx
+    mov edx, 0
+    ;eax already is number to devide
+    mov ecx, 10
+    idiv ecx
+    ;convert digit to printable
+    add edx, 48
+    mov [edi], edx
+    dec edi
+    pop edx
+it_1:
+    push edx
+    mov edx, 0
+    ;eax already is number to devide
+    mov ecx, 10
+    idiv ecx
+    ;convert digit to printable
+    add edx, 48
+    mov [edi], edx
+
+    ;pop edx into eax as its how long this string is
+    pop eax
+    ret
+
 atoi:
-    ; start at zero
+    ;start at zero
     mov eax, 0
 at_convert:
-    ; next char
+    ;next char
     movzx esi, byte [edi]
-    ; check for \n
+    ;check for \n
     cmp esi, 0xa
     je at_done
 
-    ; less than '0' is invalid
+    ;less than '0' is invalid
     cmp esi, 48
     jl errout
 
-    ; greater than '9' is invalid
+    ;greater than '9' is invalid
     cmp esi, 57
     jg errout
 
-    ; convert to actual number
+    ;convert to actual number
     sub esi, 48
-    ; multiply by 10
+    ;multiply by 10
     imul eax, 10
-    ; add to total
+    ;add to total
     add eax, esi
 
-    ; get address of next char
+    ;get address of next char
     inc edi
     jmp at_convert
 at_done:
